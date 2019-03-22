15b955f2ca6b2a40c096f1c3d8fb1b00fbb868f7
Carmen Creswell <ccreswell@adaptavist.com>
2019-03-22 16:35:20 -0500

    Get date based on date object

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,33 @@
 'use strict';
 
 const ul = document.getElementById('events');
-let date = '02/14/2019' 
-const url = 'http://people.missouristate.edu/chadkillingsworth/csc590/calendar/?date=' + date; 
+//let date = '02/14/2019'; 
+let currentDate = new Date();
+//const url = 'http://people.missouristate.edu/chadkillingsworth/csc590/calendar/?date=' + date; 
 let loadingImg = document.getElementById("loader");
+
+let back = document.getElementById("left-button");
+let forward = document.getElementById("right-button");
+let mill = currentDate.getMilliseconds();
+console.log("Milliseconds: " + mill);
+
+back.addEventListener("click", function () {
+    currentDate = (mill * 60) * 86400;
+
+})
+
+forward.addEventListener("click", function() {
+    currentDate = (mill * 60) * 86400;
+})
+
+let day = currentDate.getDate();
+let month = currentDate.getMonth() + 1;
+let year = currentDate.getFullYear();
+
+let url = 'http://people.missouristate.edu/chadkillingsworth/csc590/calendar/?date=' + month + "/" + day + "/" + year;
+console.log(currentDate);
+console.log(day);
+console.log(url);
 
 fetch(url)
 .then(function(response) {
@@ -18,7 +42,7 @@ fetch(url)
 })
 .then(function(data) {
     console.log(data);
-    
+
     loadingImg.style.display='none';
     
     for (let i = 0; i < data.feed.entry.length; i++) {
